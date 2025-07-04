1c001d2e89efe1a26c85d068505c8ac369bc776e
NTarver17 <nicktarver17@gmail.com>
2025-07-04 12:53:18 -0500

    Updated CyberFit styling, added login page, and animations

---
+++ README.md
@@ -0,0 +1,2 @@
+# CyberFit
+Collaboration project for portfolio by Marine veterans.
---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector('.get-started').addEventListener('click', () => {
+  alert("Let's get started with CyberFit!");
+});
+
+document.querySelector('.learn-more').addEventListener('click', () => {
+  alert("Here’s more about how CyberFit can help you.");
+});
