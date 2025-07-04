ccaa89abf7625c4cf3c4a3f3b38b11712fe16a2d
NTarver17 <nicktarver17@gmail.com>
2025-07-04 10:33:04 -0500

    Add Firebase configuration file

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBX0xl0Vil1Ltl7yYXhRNL9AQv0K3JhQkU",
+  authDomain: "cyberfit-f92c1.firebaseapp.com",
+  projectId: "cyberfit-f92c1",
+  storageBucket: "cyberfit-f92c1.firebasestorage.app",
+  messagingSenderId: "747025044106",
+  appId: "1:747025044106:web:7012e08267f462a335d373"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
